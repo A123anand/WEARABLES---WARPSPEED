741a30610144ec8fb9108bbc5f3a0c5cd8b31511
Amisha <amishalakshmianand@gmail.com>
2026-01-20 21:26:42 -0700

    set up

---
+++ start.py
@@ -0,0 +1,12 @@
+# ok so this is where i will be typing in notes/plans for the project
+# Warpspeed needs access to some csv files SO i am making some dummy data for testing
+# this will allow me to test the functionality without needing the actual data files 
+#it is a huge file i aint gonna lie but it will be what we work with in the end anyway
+# things to install on vs :
+# pandas
+# numpy
+# matplotlib
+# seaborn
+# sklearn
+# scipy
+import pandas as pd
