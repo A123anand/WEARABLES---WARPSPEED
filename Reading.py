da0982b4bebca8e5f2beafe7fd69fda69c64dab2
Amisha <amishalakshmianand@gmail.com>
2026-02-10 16:36:49 -0700

    read

--- Reading.py
+++ Reading.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
-import csv
+import dummy_spine_imu_angles_day.csv as dummy_data
 
-# def read_csv_file(file_path):
+def read_csv_file(file):
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file)
+    
+    # Convert the DataFrame to a NumPy array
+    data_array = df.to_numpy()
+    
+    return data_array
+
+#main function:
+if __name__ == "__main__":
+    # Example usage
+    file_path = 'dummy_spine_imu_angles_day.csv'
+    data = read_csv_file(file_path)
