db8832a2049d51b3b06059cc00c6e90602810bbe
Amisha <amishalakshmianand@gmail.com>
2026-02-10 19:47:34 -0700

    push!!

--- Reading.py
+++ Reading.py
@@ -21,12 +21,12 @@ def read_csv_file(file):
     #print(data.to_string())
     data["timestamp"] = pd.to_datetime(data["timestamp"])
     time = data["timestamp"].to_numpy()
-    upper_roll = data["imu_upper_roll_deg"].to_numpy
-    upper_pitch = data["imu_upper_pitch_deg"].to_numpy
-    upper_yaw = data["imu_upper_yaw_deg"].to_numpy
-    lower_roll = data["imu_lower_roll_deg"].to_numpy
-    lower_pitch = data["imu_lower_pitch_deg"].to_numpy
-    lower_yaw = data["imu_lower_yaw_deg"].to_numpy 
+    upper_roll = data["imu_upper_roll_deg"].to_numpy()
+    upper_pitch = data["imu_upper_pitch_deg"].to_numpy()
+    upper_yaw = data["imu_upper_yaw_deg"].to_numpy()
+    lower_roll = data["imu_lower_roll_deg"].to_numpy()
+    lower_pitch = data["imu_lower_pitch_deg"].to_numpy()
+    lower_yaw = data["imu_lower_yaw_deg"].to_numpy ()
     #.to_numpy makes them into numpy arrays which is easier to work with
 
     return time, upper_roll, upper_pitch, upper_yaw, lower_roll, lower_pitch, lower_yaw
