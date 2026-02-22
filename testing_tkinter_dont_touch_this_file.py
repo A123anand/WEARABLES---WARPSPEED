fed6a99b0b778d73a7176f6b8189797e11ee9c65
Amisha <amishalakshmianand@gmail.com>
2026-02-22 15:37:22 -0700

    tkinter trials

--- testing_tkinter_dont_touch_this_file.py
+++ testing_tkinter_dont_touch_this_file.py
@@ -1,17 +1,26 @@
 from tkinter import Tk, Frame, Button
-
+root = Tk()
+def print():
+    frame = Frame(root)
+    frame.pack(expand=True, fill="both")
+    
+    print("Button was clicked!")
 def make_a_button():
-    root = Tk()
+    
     root.title("Warpspeed Test Window")
     root.geometry("400x300")
 
     frame = Frame(root)
     frame.pack(expand=True, fill="both")
 
-    button = Button(frame, text="Geek")
-    button.pack()
 
+    # Connect the function directly to the button here
+    button = Button(frame, text="Geek", command=print)
+    button.pack(pady=20)
+
+    # This starts the window; code below this won't run until you exit
     root.mainloop()
+
 
 make_a_button()
 
