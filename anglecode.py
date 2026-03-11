fc1ea5286a5533769a1bb0ef374c3bbbfa3de802
Amisha <amishalakshmianand@gmail.com>
2026-03-10 18:36:38 -0600

    more stuff

--- anglecode.py
+++ anglecode.py
@@ -1,28 +1,8 @@
 import numpy as np
 import serial
 
-ser = serial.Serial('COM3', 115200)
+ser = serial.Serial('COM11',9600)
 
-time_data = []
-imu_1 = []
-imu_2 = []
-
-while True:
-
-    line = ser.readline().decode().strip()
-    values = [float(v) for v in line.split(',')]
-
-    t = values[0]
-    ax, ay, az = values[1:4]
-    bx, by, bz = values[4:7]
-
-    time_data.append(t)
-    imu_1.append([ax, ay, az])
-    imu_2.append([bx, by, bz])
-
-    # convert to numpy when needed
-    imu_1_np = np.array(imu_1)
-    imu_2_np = np.array(imu_2)
 
 def IMU(imu_1, imu_2):
     # number of comparable rows
@@ -40,8 +20,33 @@ def IMU(imu_1, imu_2):
         dot_product = max(min(dot_product, 1), -1)
 
         angle_deg[i] = np.degrees(np.arccos(dot_product))
-    
+    print(IMU(imu_1, imu_2))
+    # convert to numpy when needed
+    imu_1_np = np.array(imu_1)
+    imu_2_np = np.array(imu_2)
     return angle_deg
 
 # Example call
-print(IMU(imu_1, imu_2))
+
+
+
+if __name__ == "__main__":
+    
+    time_data = []
+    imu_1 = []
+    imu_2 = []
+
+
+    while True:
+
+        line = ser.readline().decode().strip()
+        values = [float(v) for v in line.split(',')]
+
+        t = values[0]
+        ax, ay, az = values[1:4]
+        bx, by, bz = values[4:7]
+
+        time_data.append(t)
+        imu_1.append([ax, ay, az])
+        imu_2.append([bx, by, bz])
+    IMU(imu_1, imu_2)
