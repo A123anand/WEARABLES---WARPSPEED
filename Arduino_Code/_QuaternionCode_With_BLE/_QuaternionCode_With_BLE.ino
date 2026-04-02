5d8f4a447af00b20ac0ebd618c37c45ea0891647
marekharland2007-commits <marekharland2007@gmail.com>
2026-04-02 10:13:20 -0600

    moved old files to old folder, Python layer is no longer necessary unless it makes website easier

--- Arduino_Code/_QuaternionCode_With_BLE/_QuaternionCode_With_BLE.ino
+++ Arduino_Code/_QuaternionCode_With_BLE/_QuaternionCode_With_BLE.ino
@@ -91,10 +91,8 @@ struct MadgwickFilter {
       q0q0 = q0*q0; q1q1 = q1*q1; q2q2 = q2*q2; q3q3 = q3*q3;
 
       s0 = _4q0*q2q2 + _2q2*ax + _4q0*q1q1 - _2q1*ay;
-      s1 = _4q1*q3q3 - _2q3*ax + 4.0f*q0q0*q1 - _2q0*ay
-         - _4q1 + _8q1*q1q1 + _8q1*q2q2 + _4q1*az;
-      s2 = 4.0f*q0q0*q2 + _2q0*ax + _4q2*q3q3 - _2q3*ay
-         - _4q2 + _8q2*q1q1 + _8q2*q2q2 + _4q2*az;
+      s1 = _4q1*q3q3 - _2q3*ax + 4.0f*q0q0*q1 - _2q0*ay - _4q1 + _8q1*q1q1 + _8q1*q2q2 + _4q1*az;
+      s2 = 4.0f*q0q0*q2 + _2q0*ax + _4q2*q3q3 - _2q3*ay - _4q2 + _8q2*q1q1 + _8q2*q2q2 + _4q2*az;
       s3 = 4.0f*q1q1*q3 - _2q1*ax + 4.0f*q2q2*q3 - _2q2*ay;
 
       recipNorm = 1.0f / sqrtf(s0*s0 + s1*s1 + s2*s2 + s3*s3);
